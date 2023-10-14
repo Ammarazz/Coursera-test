cf66bc3f182dc6e0ccbba2e1f232b1d12b874cd9
Ammarazz <146873906+Ammarazz@users.noreply.github.com>
2023-10-14 19:49:07 +0500

    Update speakegoodbye.js

--- mod-4_solution/speakegoodbye.js
+++ mod-4_solution/speakegoodbye.js
@@ -1,5 +1,9 @@
-var speakWord = "Good Bye";
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
 
-
-function speak(name) {
-  console.log(speakWord + " " + name);
+})(window);
