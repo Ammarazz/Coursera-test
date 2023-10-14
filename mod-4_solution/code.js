ebe87d21b16c61efe31bb0b885a08e7815110533
Ammarazz <146873906+Ammarazz@users.noreply.github.com>
2023-10-14 17:48:23 +0500

    Update code.js

--- mod-4_solution/code.js
+++ mod-4_solution/code.js
@@ -1,1 +1,19 @@
-mod-4
+// Starter code with an array of names
+const names = ['John', 'Jane', 'Doe', 'Alice'];
+
+// Loop through the array
+for (let i = 0; i < names.length; i++) {
+  // Get the first letter of the name
+  const firstLetter = names[i].charAt(0);
+
+  // Use libraries to print "Hello" or "Goodbye" based on the first letter
+  if (firstLetter === 'J' || firstLetter === 'j') {
+    // Call the library function to print "Goodbye"
+    goodbyeLibrary.printGoodbye(names[i]);
+  } else {
+    // Call the library function to print "Hello"
+    helloLibrary.printHello(names[i]);
+  }
+}
+
+
