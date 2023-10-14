03a90809fc0c056e940c90d07f001d43f443fcb7
Ammarazz <146873906+Ammarazz@users.noreply.github.com>
2023-10-14 21:38:05 +0500

    Update script.js

--- mod-4_solution/script.js
+++ mod-4_solution/script.js
@@ -1,10 +1,8 @@
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
-
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
   }
@@ -12,7 +10,6 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker(names[i]);
   }
 }
-
 })();
 
 
